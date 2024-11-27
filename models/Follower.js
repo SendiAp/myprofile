0df426f9dc2cab39ec2513c4e1b20f5d5a90403b
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-11-27 13:44:47 +0700

    Delete models directory

--- models/Follower.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const followerSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    followerCount: { type: Number, default: 0 }
-});
-
-// Pastikan model diekspor dengan benar
-module.exports = mongoose.model('Follower', followerSchema);
