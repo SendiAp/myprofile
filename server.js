170a4a2b8b3644351a8d688844953c4e0f734700
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-11-27 11:35:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -17,31 +16,42 @@ const uri = 'mongodb+srv://sya:sya@cluster0.sfufjbg.mongodb.net/?retryWrites=tru
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Endpoint untuk mengikuti
 app.post('/follow', async (req, res) => {
     const { username } = req.body;
 
-    let follower = await Follower.findOne({ username });
-    if (!follower) {
-        follower = new Follower({ username, followerCount: 1 });
-    } else {
-        follower.followerCount++;
-    }
+    try {
+        let follower = await Follower.findOne({ username });
+        if (!follower) {
+            follower = new Follower({ username, followerCount: 1 });
+        } else {
+            follower.followerCount++;
+        }
 
-    await follower.save();
-    res.json({ followerCount: follower.followerCount });
+        await follower.save();
+        res.json({ followerCount: follower.followerCount });
+    } catch (error) {
+        console.error('Error while following:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 // Endpoint untuk mendapatkan jumlah pengikut
 app.get('/followers/:username', async (req, res) => {
     const { username } = req.params;
-    const follower = await Follower.findOne({ username });
-    if (follower) {
-        res.json({ count: follower.followerCount });
-    } else {
-        res.json({ count: 0 });
+
+    try {
+        const follower = await Follower.findOne({ username });
+        if (follower) {
+            res.json({ count: follower.followerCount });
+        } else {
+            res.json({ count: 0 });
+        }
+    } catch (error) {
+        console.error('Error while fetching followers:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
