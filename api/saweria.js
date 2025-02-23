e3b9f03389bc40b86388a248d6729fc84807d27e
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2025-02-23 23:29:08 +0700

    Update saweria.js

--- api/saweria.js
+++ api/saweria.js
@@ -5,15 +5,19 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { userId, payload } = req.body;
 
+        if (!userId || !payload) {
+            return res.status(400).json({ error: 'Missing userId or payload' });
+        }
+
         try {
             const response = await axios.post(`https://backend.saweria.co/donations/${userId}`, payload);
-            res.status(200).json(response.data);
+            return res.status(200).json(response.data);
         } catch (error) {
-            console.error('Error:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
+            console.error('Error:', error.response ? error.response.data : error.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
     } else {
         res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
